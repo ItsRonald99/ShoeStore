eb46da45674baeee6495423afb557383c28c7ea2
ItsRonald99 <r3ma@uwaterloo.ca>
2025-07-29 00:06:38 -0400

    second

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555; 
-export const mongoDBURL = 'mongodb+srv://agentronisthe1:AyoMongoData123@shoe-store.ndg8lw6.mongodb.net/shoe-collection?retryWrites=true&w=majority';
+export const mongoDBURL = '...';
